0c43af5235ba1929174eb46168f50ca85a6f74b0
Eugene Lee <eugene097@csu.fullerton.edu>
2021-01-24 19:17:25 -0800

    Started implementing google search to try and scrape google results page

--- backend/scraper/scraper.py
+++ backend/scraper/scraper.py
@@ -2,7 +2,7 @@ import urllib
 from bs4 import BeautifulSoup
 from googlesearch import search
 req = urllib.request.Request("https://www.urbanoutfitters.com/", headers={'User-Agent' : "Magic Browser"}) 
-con = urllib.request.urlopen( req )
+con = urllib.request.urlopen(req)
 soup = BeautifulSoup(con.read(), features = "html.parser")
 
 #get company name
@@ -16,10 +16,26 @@ url = soup.find("meta", property = "og:url")
 # print("url: ", url)
 
 query = "Brands like {}".format(title)
-print(query)
+# print(query)
 
-for i in search(query, tld = "co.in", num = 10, stop = 10, pause = 2):
-    print(i)
+url_search = "https://www.google.com/search?lr=lang_en&" \
+             "q=%{}&btnG=Google+Search&tbs=0&safe=off&" \
+             "cr=''&filter=0".format(query)
+
+req = urllib.request.Request(url_search, headers={'User-Agent' : "Magic Browser"}) 
+con = urllib.request.urlopen(req)
+soup = BeautifulSoup(con.read(), features = "html.parser")
+
+print(url_search)
+# print(soup.prettify())
 
 
+# Fill in search bar with <query>
+# hit google search
+# scrap class i8Z77e
+    # return all items under that class
+
+
+# for i in search(query, tld = "co.in", num = 10, stop = 10, pause = 2):
+#     print(i)
 
